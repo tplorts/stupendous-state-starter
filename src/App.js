fea7bf3227e95fcff4a20ab731c30d41b21a95ca
Ted Lorts <tplorts@gmail.com>
2019-11-04 20:49:09 -0800

    WEDUX!!!

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
-import React, { useState } from "react";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { createStore } from "redux";
 import "./App.css";
 
+const initialState = {
+  text: "",
+  list: []
+};
+
+// let state = {};
+
+// function dispatch(action) {
+//   state = reducer(state, action);
+//   // update all subscribers
+// }
+
+function reducer(priorState = initialState, action) {
+  if (action.type === "SET_TEXT") {
+    return {
+      text: action.text,
+      list: priorState.list
+    };
+  }
+
+  if (action.type === "ADD_ITEM") {
+    return {
+      text: priorState.text,
+      list: [...priorState.list, action.item]
+    };
+  }
+
+  return priorState;
+}
+
+export const store = createStore(reducer);
+
 function App() {
-  const [text, setText] = useState("");
-  const [list, setList] = useState([]);
+  const dispatch = useDispatch();
+
+  // const [text, setText] = useState("");
+  const text = useSelector(state => state.text);
 
   function addAThing() {
-    setList([...list, text]);
-    setText("");
+    // setList([...list, text]);
+    dispatch({
+      type: "ADD_ITEM",
+      item: text
+    });
+
+    // setText("");
+    dispatch({
+      type: "SET_TEXT",
+      text: ""
+    });
   }
 
   return (
@@ -16,16 +61,29 @@ function App() {
         type="text"
         value={text}
         onChange={e => {
-          setText(e.target.value);
+          // setText(e.target.value);
+          dispatch({
+            type: "SET_TEXT",
+            text: e.target.value
+          });
         }}
       />
       <button onClick={addAThing}>Weeeeeeee</button>
-      <ul>
-        {list.map(item => (
-          <li>{item}</li>
-        ))}
-      </ul>
+      <List />
     </div>
+  );
+}
+
+function List() {
+  // const [list, setList] = useState([]);
+  const list = useSelector(state => state.list);
+
+  return (
+    <ul>
+      {list.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
   );
 }
 
